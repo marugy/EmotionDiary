5e81e6267e58e97225cd1ea12a356c72eb186020
marugy <gyulife7301@naver.com>
2022-10-16 20:43:22 +0900

    optimization

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 import DiaryItem from "./DiaryItem";
@@ -13,7 +13,7 @@ const filterOptionList = [
   { value: "bad", name: "안좋은 감정만" },
 ];
 
-const ControlMenu = ({ value, onChange, optionList }) => {
+const ControlMenu = React.memo(({ value, onChange, optionList }) => {
   //value 셀렉트가 뭘 선택하고 있는지, 선택시바꿀함수, opritonList select함수에 들어갈 내용
   return (
     <select
@@ -28,7 +28,7 @@ const ControlMenu = ({ value, onChange, optionList }) => {
       ))}
     </select>
   );
-};
+});
 
 const DiaryList = ({ diaryList }) => {
   const navigate = useNavigate();
